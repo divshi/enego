cc98c9c5677aee99206e432d88baa4ced929c1ff
Divya Shrivastava <shridivya893@gmail.com>
2024-11-12 08:30:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
   );
